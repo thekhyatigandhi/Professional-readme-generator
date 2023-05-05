e70e1b724fd764ac0876c9ec6176fd640cbf9fd4
Khyati Gandhi <125392517+thekhyatigandhi@users.noreply.github.com>
2023-05-05 16:44:46 -0400

    Changes in license

--- main/utils/generateMarkdown.js
+++ main/utils/generateMarkdown.js
@@ -69,7 +69,7 @@ ${data.installation}
 ${data.usage}
 
 ## License 
-${data.license}
+${renderLicenseLink(data.license)}
 * As this license list was not comprehensive, if you need another license, use the contact information below to ask for license to be added. 
 
 ## Contributing 
